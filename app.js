dc38773257fd7aee4ed06b13893dc3414d91b326
King <kingsjacobfrancis@gmail.com>
2022-07-22 14:08:24 +0400

    cors updated

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors({
 }))
 app.set('trust proxy', 1)
 app.use(session({
-    // store: MongoStore.create({mongoUrl: url}),
+    store: MongoStore.create({mongoUrl: url}),
     secret: '89050a617a0741cb3d34a87f48f97b2e',
     resave: true,
     saveUninitialized: true,
